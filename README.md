dd00378d1c0f8cb792fad016d7cb3ff87a3e0baa
Paul BONCAR <80626421+n0ct3ri4@users.noreply.github.com>
2021-09-24 08:33:25 +0400

    Add base files via upload
    - index.js
    - package.json
    - README.md

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# tokenmgr
-Create tokens and manage thme from your Node!
+<div align="center">
+  <a href="https://npmjs.com/package/tokenmgr">
+    <img src="https://img.shields.io/badge/npm-available-green?style=for-the-badge">
+  </a>
+
+  <a href="https://npmjs.com/package/tokenmgr/versions">
+    <img src="https://img.shields.io/badge/version-1.0.0-blue?style=for-the-badge">
+  </a>
+</div>
+
+<hr>
+
+## 🚀 Get started!
+
+The first step is to import the module in your node.
+
+```js
+const tokenmgr = require("tokenmgr");
+```
+
+Then, initialize a new instance of the manager.
+
+```js
+const tm = new tokenmgr();
+```
+
+## 👩‍💻 How to create tokens?
+
+You have to a single line to create a token.
+
+```js
+tm.create(64, {});
+```
+
+To add creation options, specify them into the braces.
+
+```js
+tm.create(64, {
+  avoidNumbers: true,
+  allUpperCase: true,
+  allLowerCase: true,
+});
+```
+
+> To disable an option, you just have to remove it from the table.
+
+> All options are disabled by default.
+
+## 💾 How to store them ?
+
+Just use the `tm.store()` function.
+
+```js
+tm.store("name", token);
+
+// You can create a token directly into the 'tm.store()' function.
+tm.store("name", token);
+```
+ 
---
+++ index.js
@@ -0,0 +1,123 @@
+module.exports = class {
+  tokens = [];
+
+  /**
+   * Create a token.
+   * @param {number} length Length of the token.
+   * @param {{allUpperCase: boolean, allLowerCase: boolean, avoidNumbers: boolean}} options Token-gen options.
+   */
+  create(
+    length = 64,
+    options = {
+      allLowerCase: false,
+      allUpperCase: false,
+      avoidNumbers: false,
+    }
+  ) {
+    if (!length || !options) return false;
+    let output = "";
+    let chars = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+    ];
+
+    if (options.avoidNumbers) {
+      chars = chars.slice(0, chars.length - 10);
+    }
+
+    try {
+      for (let i = 0; i < length; i++) {
+        if (options.allLowerCase) {
+          output +=
+            chars[Math.floor(Math.random() * chars.length)].toLowerCase();
+        } else if (options.allUpperCase) {
+          output +=
+            chars[Math.floor(Math.random() * chars.length)].toUpperCase();
+        } else {
+          output += chars[Math.floor(Math.random() * chars.length)];
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      return output;
+    }
+  }
+
+  /**
+   * Store a token and assign it to a name.
+   * @param {string} name Name of the token.
+   * @param {string} token Token to store.
+   */
+  store(name, token) {
+    this.tokens[name] = token;
+  }
+
+  /**
+   * Unstore a token.
+   * @param {string} name Name of the token.
+   */
+  del(name) {
+    this.tokens[name] = null;
+  }
+};
